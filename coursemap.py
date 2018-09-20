62d6a08c1d7e6f415cde0d8511c80ed52b22178a
Max Green <mtg.greenfam@gmail.com>
2018-09-20 16:40:57 -0500

    Minor Changes

--- coursemap.py
+++ coursemap.py
@@ -39,7 +39,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--init', help='Initialize our SQLite database.', action='store_true')
 
-# Add more Arguments
+## TODO:  Add more Arguments
 
 args = parser.parse_args()
 # args = parser.parse_args(['--init'])
