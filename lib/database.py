b1b40f975509d2be87258a30b1550b988eea3915
Steve Helms <steve@cadenrock.com>
2018-09-22 10:23:54 -0500

    Creating new database library file all functions to live in for consistency.

---
+++ lib/database.py
@@ -0,0 +1,40 @@
+import os
+import sqlite3
+import logging
+
+def sqlite_connect(db):
+    # TODO: Check for default sqlite database. if not exist, error and exit
+    # QUESTION: Do we auto-create the database and schema if the database does not exist? If so, we just call our create_sqlite_tables method.
+
+    # Connect to DB and return
+    conn = sqlite3.connect(db)
+    return conn
+
+
+def create_sqlite_tables():
+    sqlfile = open("doc/sql.txt", "r")
+    sqltxt = sqlfile.read()
+    c.executescript(sqltxt)
+
+
+def create_course(row):
+    logging.debug(row['Course']+' '+row['Department']+' '+row['Grade Level']+' '+row['Credits'])
+    # TODO: Complete the database method.
+
+
+def create_student(row):
+    (last_name, first_name) = row['LastName, FirstName'].split(',')
+    logging.debug(row['Student ID (System)']+','+
+        last_name.strip()+','+first_name.strip()+','+row['Grade Level'])
+    # TODO: Complete the database method.
+
+
+def create_student_course(student_id,course_id,credits):
+    logging.debug(student_id+' '+course_id+' '+credits)
+    # TODO: Complete the database method.
+
+
+# When our module is loaded, connect to the database and create a cursor
+# object so we can use the connection.
+conn = sqlite_connect()
+c = conn.cursor()
