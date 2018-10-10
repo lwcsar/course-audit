577f612f115b297f0198bb232579c7b727fa74a5
Max Green <mtg.greenfam@gmail.com>
2018-10-10 13:55:40 -0500

    Started Output

--- lib/output.py
+++ lib/output.py
@@ -1,3 +1,5 @@
+from reportlab.pdfgen import canvas
+
 '''
 OUTPUT
 
@@ -6,3 +8,13 @@ The program output will initially be in 2 forms.
     2) PDF saved to disk
 
 '''
+
+class OutputPDF:
+    def __init__(self):
+        c = canvas.Canvas("Student_Reports.pdf", pagesize=letter)
+        c.drawString(30,750,"Student Reports:")
+        
+    
+        
+    def savePDF(self):
+        c = save()
